4fa40ac004f2cd05222599d257962827dbae9116
Saurav-Mistri <sauravmistri21@gmail.com>
2025-03-01 16:20:58 +0530

    parsing parameters to the url

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-const smMiddleware = (req, res, next)=>{
-  console.log("req");
-  console.log(req);
-  // to execute next middleware we use next() 
-  next();
-}
+// const smMiddleware = (req, res, next)=>{
+//   console.log("req");
+//   console.log(req);
+//   // to execute next middleware we use next() 
+//   next();
+// }
 
 app.use(express.static(path.join(__dirname, "public")))
 // app.use(smMiddleware)
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+app.get('/hello/:name', (req, res) => {
+  res.send('Hello World! ' + req.params.name)
 })
 
 app.get('/about', (req, res) => {
-    // res.send('about!')
-    // res.sendFile(path.join(__dirname, 'index.html'))
-    // res.status(200)
-    res.json({"SM": 21})
-  })
+  // res.send('about!')
+  // res.sendFile(path.join(__dirname, 'index.html'))
+  // res.status(200)
+  res.json({ "SM": 21 })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
